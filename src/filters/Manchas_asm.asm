e9d434b7b4d9db4c5ef78f794188523c90ea12d3
Gastón Máspero <gmaspero@dc.uba.ar>
2019-05-12 21:32:55 -0300

    Removí converts innecesarios, reducí la cantidad de operaciones aritméticas, saqué los "finit" en cada iteración y cambié fst por fstp. Ahora por fin la implementación en ASM es más rápida que la de C.

--- src/filters/Manchas_asm.asm
+++ src/filters/Manchas_asm.asm
@@ -9,8 +9,6 @@ dos: dd 2.0, 2.0, 2.0, 2.0
 pi: dd 3.14159, 3.14159, 3.14159, 3.14159 
 cincuenta: dd 50.0, 50.0, 50.0, 50.0
 veinticinco: dd 25.0, 25.0, 25.0, 25.0
-;mascara: db 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0xff, 0xff, 0x02, 0x03, 0x02, 0x03, 0x02, 0x03, 0xff, 0xff 
-;mascara2: db 0x04, 0x05, 0x04, 0x05, 0x04, 0x05, 0xff, 0xff, 0x06, 0x07, 0x06, 0x07, 0x06, 0x07, 0xff, 0xff    
 
 section .text
 
@@ -33,16 +31,16 @@ Manchas_asm:
 	movdqu xmm10, [pi]
 	pxor xmm12, xmm12; Para que guarde 0 y unpackear más adelante
 	mulps xmm10, xmm5
-	;movdqu xmm14, [mascara]
-	;movdqu xmm15, [mascara]
 	mov r14d, edx; R14d para WIDTH
 	mov r15d, ecx; R15d para HEIGHT
 	xor rbx, rbx
 	mov ebx, dword [rbp + 16]; ebx = n
-	xor edx, edx
-	xor rcx, rcx
+	cvtsi2ss xmm13, ebx
+	shufps xmm13, xmm13, 0
+	divps xmm10, xmm13; xmm10 == ||2 * PI / n| 2 * PI / n | 2 * PI / n | 2 * PI / n||
 	xor r12, r12; Iterador filas - i
 	xor r13, r13; Iterador columnas - j
+	finit
 	.rowLoop:
 		.colLoop:
 			mov ecx, 3
@@ -50,41 +48,40 @@ Manchas_asm:
 				mov eax, r13d
 				xor rdx, rdx
 				div ebx
-				cvtsi2ss xmm0, edx
+				movd xmm1, edx
+				movss xmm0, xmm1
 				pslldq xmm0, 4
 				inc r13
 				loop .loopJmodN
 			mov eax, r13d
 			xor rdx, rdx
 			div ebx
-			cvtsi2ss xmm0, edx
+			movd xmm1, edx
+			movss xmm0, xmm1
+			cvtdq2ps xmm0, xmm0
 			inc r13
-			;xmm0 == ||(float)(j mod n) | (float)((j + 1) mod n) | (float)((j + 2) mod n) | (float)((j + 3) mod n)||	
-			cvtsi2ss xmm1, ebx; xmm1 = (float) n
-			shufps xmm1, xmm1, 0
-			divps xmm0, xmm1; xmm0 == ||(float)(j mod n) / n | (float)((j + 1) mod n) / n|
-			;(float)((j + 2) mod n) / n | (float)((j + 3) mod n) / n||
+			;xmm0 == ||(float)(j mod n) | (float)((j + 1) mod n) | (float)((j + 2) mod n) | (float)((j + 3) mod n)||
 			mulps xmm0, xmm10; xmm0 == ||(float)(j mod n) / n * 2 * PI| (float)((j + 1) mod n) / n * 2 * PI|
 			;(float)((j + 2) mod n) / n * 2 * PI| (float)((j + 3) mod n) / n * 2 * PI||
-			;Aplico cos
+
+			;Aplico cos: Lo hago en forma de ciclo para reducir instrucciones
+			;La última iteración se hace a mano para no shiftear de más
 			;En xmm2 voy a acumular los cos
-			mov ecx, 3
+			mov ecx, 3; 3 iteraciones
 			.loopCos:
-				finit
 				movd [rbp - 8], xmm0
 				fld dword [rbp - 8]
 				fcos
-				fst dword [rbp - 8]
-				movd xmm3, [rbp - 8]
+				fstp dword [rbp - 8]
+				movd xmm3, [rbp - 8];Recordar que movd hace zero clear, por eso uso a xmm3 de intermediario
 				movss xmm2, xmm3
 				psrldq xmm0, 4
 				pslldq xmm2, 4
 				loop .loopCos
-			finit 
 			movd [rbp - 8], xmm0
 			fld dword [rbp - 8]
 			fcos
-			fst dword [rbp - 8]
+			fstp dword [rbp - 8]
 			movd xmm3, [rbp - 8]
 			movss xmm2, xmm3
 			;Reordeno xmm2 en xmm0
@@ -97,13 +94,11 @@ Manchas_asm:
 			xor rdx, rdx
 			div ebx
 			cvtsi2ss xmm2, edx
-			divss xmm2, xmm1; xmm2 == (float)(i mod n) / n
 			mulss xmm2, xmm10; xmm2 == (float)(i mod n) / n * 2 * pi
-			finit
 			movd [rbp - 8], xmm2
 			fld dword [rbp - 8]
 			fsin
-			fst dword [rbp - 8]
+			fstp dword [rbp - 8]
 			movd xmm2, [rbp - 8]
 			shufps xmm2, xmm2, 0
 			;xmm2 = ||sin((float)(i mod n) / n * 2 * pi) | sin((float)(i mod n) / n * 2 * pi) 
@@ -111,20 +106,8 @@ Manchas_asm:
 			mulps xmm0, xmm2
 			mulps xmm0, xmm7
 			subps xmm0, xmm4
-			mov ecx, 3
-			.loopIntCast:
-				cvtss2si r11d, xmm0
-				movd xmm3, r11d
-				movss xmm2, xmm3; Truco para evitar el zero clear de movd, no se me ocurrió otra solución
-				pslldq xmm2, 4
-				psrldq xmm0, 4
-				loop .loopIntCast
-			cvtss2si r11d, xmm0
-			movd xmm3, r11d
-			movss xmm2, xmm3; Truco para evitar el zero clear de movd, no se me ocurrió otra solución
-			pshufd xmm0, xmm2, 0x1b
+			cvtps2dq xmm0, xmm0
 			;xmm0 == ||x_0 | x_1 | x_2 | x_3||
-			;---------------------------------
 			movdqu xmm1, [rdi];xmm1 == ||a3|r3|g3|b3||a2|r2|g2|b2||a1|r1|g1|b1||a0|r0|g0|b0||
 			movdqu xmm3, xmm1
 			punpcklbw xmm3, xmm12
@@ -163,7 +146,6 @@ Manchas_asm:
 			pand xmm6, xmm11
 			packuswb xmm3, xmm6
 			movdqu [rsi], xmm3
-			;---------------------------------
 			add rdi, 16
 			add rsi, 16
 			cmp r13d, r14d
