ed8a443e29b36855a48186db1be9cef093647e89
Gastón Máspero <gmaspero@dc.uba.ar>
2019-05-17 02:01:22 -0300

    Ahora al correr tp2 tira el output en formato csv, y dice tmb el desvío std

--- src/tp2.c
+++ src/tp2.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <math.h>
 
 #include "tp2.h"
 #include "helper/tiempo.h"
@@ -38,12 +39,13 @@ int main( int argc, char** argv ) {
     procesar_opciones(argc, argv, &config);
     
     // Imprimo info
+    /*
     if (!config.nombre) {
         printf ( "Procesando...\n");
         printf ( "  Filtro             : %s\n", config.nombre_filtro);
         printf ( "  Implementación     : %s\n", C_ASM( (&config) ) );
         printf ( "  Archivo de entrada : %s\n", config.archivo_entrada);
-    }
+    }*/
 
     snprintf(config.archivo_salida, sizeof  (config.archivo_salida), "%s/%s.%s.%s%s.bmp",
             config.carpeta_salida, basename(config.archivo_entrada),
@@ -72,34 +74,46 @@ filtro_t* detectar_filtro(configuracion_t *config) {
     return NULL; // avoid C warning
 }
 
-void imprimir_tiempos_ejecucion(unsigned long long int start, unsigned long long int end, int cant_iteraciones) {
-    unsigned long long int cant_ciclos = end-start;
-
-    printf("Tiempo de ejecución:\n");
-    printf("  Comienzo                          : %llu\n", start);
-    printf("  Fin                               : %llu\n", end);
-    printf("  # iteraciones                     : %d\n", cant_iteraciones);
-    printf("  # de ciclos insumidos totales     : %llu\n", cant_ciclos);
-    printf("  # de ciclos insumidos por llamada : %.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
+void imprimir_tiempos_ejecucion(unsigned long long ciclos_totales, unsigned long long ciclos_media, 
+                                unsigned long long desvio_estandar, configuracion_t *config) {
+    
+    printf("Filtro, Implementación, Imagen, Iteraciones, Ciclos Totales, Ciclos por Llamada, Desvío Std\n");
+    printf("%s, %s, %s, %d, %llu, %llu, %llu\n", config->nombre_filtro, C_ASM( (config) ), config->archivo_entrada,
+                                config->cant_iteraciones, ciclos_totales, ciclos_media, desvio_estandar);
 }
 
 void correr_filtro_imagen(configuracion_t *config, aplicador_fn_t aplicador) {
     imagenes_abrir(config);
 
     unsigned long long start, end;
-
+    unsigned long long total_start, total_end;
     imagenes_flipVertical(&config->src, src_img);
     imagenes_flipVertical(&config->dst, dst_img);
-    MEDIR_TIEMPO_START(start)
+    unsigned long long ciclos_por_iteracion[config->cant_iteraciones];
+    unsigned long long ciclos_totales = 0;
+    unsigned long long ciclos_media = 0;
+    MEDIR_TIEMPO_START(total_start);
     for (int i = 0; i < config->cant_iteraciones; i++) {
+            MEDIR_TIEMPO_START(start);
             aplicador(config);
+            MEDIR_TIEMPO_STOP(end);
+            ciclos_por_iteracion[i] = end - start;
+            ciclos_totales += end - start;
     }
-    MEDIR_TIEMPO_STOP(end)
+    MEDIR_TIEMPO_STOP(total_end);
+    ciclos_media = ciclos_totales / config->cant_iteraciones;
+    unsigned long long error_cuadratico = 0;
+    for(int i = 0; i < config->cant_iteraciones; ++i){
+        long long dif = ciclos_por_iteracion[i] - ciclos_media;
+        error_cuadratico += dif * dif;
+    }
+    error_cuadratico = error_cuadratico / (config->cant_iteraciones == 1? 1 : config->cant_iteraciones - 1);
+    unsigned long long desvio_estandar = (unsigned long long) sqrt(error_cuadratico);
     imagenes_flipVertical(&config->dst, dst_img);
 
     imagenes_guardar(config);
     imagenes_liberar(config);
-    imprimir_tiempos_ejecucion(start, end, config->cant_iteraciones);
+    imprimir_tiempos_ejecucion(ciclos_totales, ciclos_media, desvio_estandar, config);
 }
 
 
