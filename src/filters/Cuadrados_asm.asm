e95a9b0d4a27f68390f39d685cf415df2e41085b
gbarbieri <gbarbieri@dc.uba.ar>
2019-05-22 18:33:37 -0300

    cuadrados por columnas

--- src/filters/Cuadrados_asm.asm
+++ src/filters/Cuadrados_asm.asm
@@ -1,5 +1,5 @@
-
 section .rodata
+ALIGN 16
 mask: db 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xFF
 maskzeros: db 0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF
 masktest: dq 0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF
@@ -99,7 +99,9 @@ Cuadrados_asm:
 	mov r11,rdx							 ;r11 es width
 	mov r13,rdx                          ;uso r13 para la macro, pues usa rdx
 	sub r11,4
-	mov r12,4                            ;r12 es i                  
+	mov r12,4                            ;r12 es i 
+    movdqa xmm3,[mask]        
+    movdqa xmm4,[maskzeros]         
     .loop_i:
         cmp r12,r10
         je .fin_i
@@ -129,7 +131,7 @@ Cuadrados_asm:
             movdqu xmm1,xmm0
             pslldq xmm1,4                    ;xmm1: max(max2,max4) | ~ | ~ | ~ 
             pmaxub xmm0,xmm1                 ;xmm0: max(max(max2,max4) , max(max1,max3)) | ~ | ~ | ~
-            por xmm0,[mask]
+            por xmm0,xmm3
             psrldq xmm0,12
             map r12,rbx,r13,r15
             movd [rsi+r15],xmm0                              
@@ -151,7 +153,7 @@ Cuadrados_asm:
     			je .fin_l
 
     			map r12,rbx,r13,r15
-    			movdqu xmm0,[maskzeros]
+    			movdqu xmm0,xmm4
     			movdqu [rsi+r15],xmm0
 
     			add rbx,4
@@ -166,7 +168,7 @@ Cuadrados_asm:
     	cmp r12,r10                          ;r10 es height -4
     	je .fin_m
  		map r12,r11,r13,r15                  ;m['i'][with-4]
-		movdqu xmm0,[maskzeros]
+		movdqu xmm0,xmm4
 		movdqu [rsi+r15],xmm0
     	inc r12
     	jmp .loop_m
@@ -182,7 +184,7 @@ Cuadrados_asm:
     			je .fin_o
 
     			map r12,rbx,r13,r15
-    			movdqu xmm0,[maskzeros]
+    			movdqu xmm0,xmm4
     			movdqu [rsi+r15],xmm0
 
     			add rbx,4
@@ -198,7 +200,7 @@ Cuadrados_asm:
     	je .fin_p
     	mov r11,0
  		map r12,r11,r13,r15                  ;m['i'][with-4]
-		movdqu xmm0,[maskzeros]
+		movdqu xmm0,xmm4
 		movdqu [rsi+r15],xmm0
     	inc r12
     	jmp .loop_p
